64b6ebcaba4959ae8f5993a329276f9e70d74b5a
lqk <1501036045@qq.com>
2026-03-09 17:46:19 +0800

    优化项目目录结构

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Vue Mix</div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
+
+const isMobile = ref(false);
+
+const checkIsMobile = () => {
+  isMobile.value = window.innerWidth <= 768;
+};
+
+onMounted(() => {
+  document.title = "Vue Bits - Animated UI Components For Vue";
+  window.scrollTo(0, 0);
+  checkIsMobile();
+  window.addEventListener("resize", checkIsMobile);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", checkIsMobile);
+});
+</script>
